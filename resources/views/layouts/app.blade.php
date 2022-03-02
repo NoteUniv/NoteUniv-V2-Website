aa8553bb808e932aa1513197847d443bf553e427
QuentiumYT <quentin.lienhardt@gmail.com>
2022-03-02 17:22:06 +0100

    Split assets + fix Livewire asset URL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
         @livewireStyles
 
         <!-- Scripts -->
+        <script src="{{ mix('js/manifest.js') }}" defer></script>
+        <script src="{{ mix('js/vendor.js') }}" defer></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
