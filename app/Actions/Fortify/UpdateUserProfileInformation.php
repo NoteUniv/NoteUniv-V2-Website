43bb2528c316fad75c12c9a73aa4226a1e3a646a
QuentiumYT <quentin.lienhardt@gmail.com>
2022-03-09 17:15:19 +0100

    Changed fields + update user information

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -19,21 +19,16 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     public function update($user, array $input)
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
+            'student_id' => ['required', 'digits:8'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
-
-        if (isset($input['photo'])) {
-            $user->updateProfilePhoto($input['photo']);
-        }
 
         if ($input['email'] !== $user->email &&
             $user instanceof MustVerifyEmail) {
             $this->updateVerifiedUser($user, $input);
         } else {
             $user->forceFill([
-                'name' => $input['name'],
+                'student_id' => $input['student_id'],
                 'email' => $input['email'],
             ])->save();
         }
@@ -49,7 +44,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     protected function updateVerifiedUser($user, array $input)
     {
         $user->forceFill([
-            'name' => $input['name'],
+            'student_id' => $input['student_id'],
             'email' => $input['email'],
             'email_verified_at' => null,
         ])->save();
