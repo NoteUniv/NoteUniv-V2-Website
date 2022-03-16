fd73bd4e7526b48590f0b5fc4d729e73c837a0f4
QuentiumYT <quentin.lienhardt@gmail.com>
2022-03-17 00:12:59 +0100

    Rewrote all seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Jetstream\Features;
 
 class UserFactory extends Factory
@@ -24,12 +24,12 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $studentId = $this->faker->unique()->randomNumber(8, true);
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'student_id' => $studentId,
+            'email' => preg_replace('/@example\..*/', '@etu.unistra.fr', $this->faker->unique()->safeEmail),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => Hash::make($studentId),
         ];
     }
 
@@ -54,14 +54,14 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam()
     {
-        if (! Features::hasTeamFeatures()) {
+        if (!Features::hasTeamFeatures()) {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
                 ->state(function (array $attributes, User $user) {
-                    return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
+                    return ['name' => $user->name . '\'s Team', 'user_id' => $user->id, 'personal_team' => true];
                 }),
             'ownedTeams'
         );
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,12 @@ namespace Database\Seeders;
 
 use App\Imports\MeccImport;
 use App\Models\Grade;
+use App\Models\Mecc;
 use App\Models\User;
 use App\Models\UserGrade;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Facades\Excel;
 
 class DatabaseSeeder extends Seeder
@@ -19,18 +21,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
+        UserGrade::truncate();
+        User::truncate();
+        Grade::truncate();
+        Mecc::truncate();
+
         User::create([
-            'email' => 'admin@noteuniv.fr',
+            'email' => 'bollender@unistra.fr',
             'is_admin' => true,
             'is_ranked' => false,
-            'password' => Hash::make('admin'),
+            'password' => Hash::make('bollender'),
         ]);
+        User::factory(10)->unverified()->create();
 
         $exampleMecc = 'https://raw.githubusercontent.com/NoteUniv/NoteUniv-Excel/main/dweb_mecc.xlsx';
-        copy($exampleMecc, storage_path('app/public/mecc/dweb_mecc.xlsx'));
-        Excel::import(new MeccImport, storage_path('app/public/mecc/dweb_mecc.xlsx'));
+        copy($exampleMecc, storage_path('app/public/dweb_mecc.xlsx'));
+        Excel::import(new MeccImport, storage_path('app/public/dweb_mecc.xlsx'));
 
-        // Grade::factory(10)->create();
-        UserGrade::factory(10)->create();
+        Grade::factory(10)->create();
+
+        Grade::All()->each(function (Grade $grade) {
+            User::All()->each(function ($user) use ($grade) {
+                if ($user->student_id !== null) {
+                    UserGrade::factory()->create([
+                        'student_id' => $user->student_id,
+                        'grade_id' => $grade->id,
+                    ]);
+                }
+            });
+        });
+
+        echo 'Created a few users, grades and grades for students.' . PHP_EOL;
+        echo 'You can now login with:' . PHP_EOL;
+        $user = User::where('student_id', '!=', null)->inRandomOrder()->first();
+        echo 'email: ' . $user->email . PHP_EOL;
+        echo 'password: ' . $user->student_id . PHP_EOL;
     }
 }
