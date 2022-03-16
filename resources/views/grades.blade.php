b923d371c1add3273c56605e6734efa4e76b0c70
YacovaHashigori <eloiantz@gmail.com>
2022-03-16 22:33:26 +0100

    update: tables responsive

--- resources/views/grades.blade.php
+++ resources/views/grades.blade.php
@@ -18,98 +18,120 @@
                 </span>
             </div>
             <div x-cloak x-show="isOpen" class="flex flex-col gap-y-6 px-4 pb-4 pt-2 xl:px-6 xl:pb-6 xl:pt-2">
-                <div class="relative" x-data="{isOpen: false}" :class="{'overflow-x-auto': isOpen}">
-                    <span
-                        class="hidden xl:inline absolute right-6 top-4 w-4 h-4 text-white transform transition-transform duration-200 pointer-events-none"
-                        :class="{'-rotate-180': isOpen}">@svg(chevron-down)</span>
-                    <table class="table w-full border-collapse">
-                        <thead>
-                            <tr class="h-12 text-base bg-nu-primary text-white cursor-pointer"
-                                @click="isOpen = !isOpen">
-                                <th
-                                    class="font-semibold text-left px-4 w-3/6 max-w-0 overflow-hidden overflow-ellipsis">
-                                    Very long name</th>
-                                <th class="font-semibold px-4 w-1/6" :class="{'min-w-[100px]': isOpen}">
-                                    {{ __('Student') }}</th>
-                                <th class="font-semibold px-4 w-2/6" :class="{'min-w-[250px]': isOpen}">
-                                    {{ __('Class') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr x-cloak x-show="isOpen">
-                                <td colspan="3">
-                                    <table class="w-full border-collapse border-l border-b border-r border-nu-gray-200">
-                                        <thead class="w-full">
-                                            <tr class="h-12 bg-nu-gray-100 text-sm text-nu-primary">
-                                                <th class="font-semibold text-left px-4 w-3/6">{{ __('Work name') }}
-                                                </th>
-                                                <th class="font-semibold px-4 w-1/6 min-w-[100px]">{{ __('Grade') }}
-                                                </th>
-                                                <th class="font-semibold px-4 w-2/6 min-w-[250px]">
-                                                    <div class="flex">
-                                                        <span class="w-full">{{ __('Average') }}</span>
-                                                        <span class="w-full">Min</span>
-                                                        <span class="w-full">Max</span>
-                                                    </div>
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php for ($i = 0; $i < 3; $i++) : ?>
-                                            <tr class="h-12 text-sm">
-                                                <td class="border border-nu-gray-200 px-4">
-                                                    <span>Assesment on Behavior & Leadership</span>
-                                                    <button
-                                                        class="relative inline-block ml-2 w-5 leading-5 bg-nu-secondary rounded-full cursor-pointer"
-                                                        x-data="{isInfoOpen: false}" @click="isInfoOpen = true">
-                                                        <span class="text-white font-light text-xs text-center">i</span>
-                                                        <div class="absolute w-max max-w-[180px] -right-2 top-1/2 translate-x-full -translate-y-1/2 text-sm text-nu-primary bg-white rounded-md border-2 border-nu-secondary p-4"
-                                                            x-show="isInfoOpen" @click.stop @click="isInfoOpen = false"
-                                                            @click.outside="isInfoOpen = false">
-                                                            Content for modal <?php echo $i; ?>
+                <div x-data="{isOpen: false, desktopVersion: false}">
+                    <div class="relative" :class="{'overflow-x-auto': isOpen}">
+                        <span
+                            class="hidden xl:inline absolute right-6 top-4 w-4 h-4 text-white transform transition-transform duration-200 pointer-events-none"
+                            :class="{'-rotate-180': isOpen}">@svg(chevron-down)</span>
+                        <table class="table w-full border-collapse whitespace-nowrap">
+                            <thead>
+                                <tr class="h-12 text-base bg-nu-primary text-white cursor-pointer"
+                                    @click="isOpen = !isOpen; desktopVersion = false">
+                                    <th
+                                        class="font-semibold text-left px-4 w-3/6 max-w-0 overflow-hidden overflow-ellipsis">
+                                        Very long name</th>
+                                    <th class="font-semibold px-4 w-1/6" :class="{'min-w-[100px]': isOpen}">
+                                        {{ __('Student') }}</th>
+                                    <th class="font-semibold px-4 w-2/6 hidden xl:table-cell"
+                                        :class="{'min-w-[250px]': isOpen, '!table-cell': desktopVersion}">
+                                        {{ __('Class') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr x-cloak x-show="isOpen">
+                                    <td colspan="3">
+                                        <table
+                                            class="w-full border-collapse border-l border-b border-r border-nu-gray-200">
+                                            <thead class="w-full">
+                                                <tr class="h-12 bg-nu-gray-100 text-sm text-nu-primary">
+                                                    <th class="font-semibold text-left px-4 w-3/6">
+                                                        {{ __('Work name') }}
+                                                    </th>
+                                                    <th class="font-semibold px-4 w-1/6 min-w-[100px]">
+                                                        {{ __('Grade') }}
+                                                    </th>
+                                                    <th class="font-semibold px-4 w-2/6 min-w-[250px] hidden xl:table-cell"
+                                                        :class="{'!table-cell': desktopVersion}">
+                                                        <div class="flex">
+                                                            <span class="w-full">{{ __('Average') }}</span>
+                                                            <span class="w-full">Min</span>
+                                                            <span class="w-full">Max</span>
                                                         </div>
-                                                    </button>
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php for ($i = 0; $i < 3; $i++) : ?>
+                                                <tr class="h-12 text-sm">
+                                                    <td class="border border-nu-gray-200 px-4 max-w-0 xl:max-w-none overflow-hidden xl:overflow-visible overflow-ellipsis"
+                                                        :class="{'max-w-none overflow-visible': desktopVersion}">
+                                                        <span>Assesment
+                                                            on Behavior &
+                                                            Leadership</span>
+                                                        <button
+                                                            class="relative ml-2 w-5 leading-5 bg-nu-secondary rounded-full cursor-pointer hidden xl:inline-block"
+                                                            :class="{'!inline-block':desktopVersion}"
+                                                            x-data="{isInfoOpen: false}" @click="isInfoOpen = true">
+                                                            <span
+                                                                class="text-white font-light text-xs text-center">i</span>
+                                                            <div class="absolute w-max max-w-[180px] -right-2 top-1/2 translate-x-full -translate-y-1/2 text-sm text-nu-primary bg-white rounded-md border-2 border-nu-secondary p-4"
+                                                                x-show="isInfoOpen" @click.stop
+                                                                @click="isInfoOpen = false"
+                                                                @click.outside="isInfoOpen = false">
+                                                                Content for modal <?php echo $i; ?>
+                                                            </div>
+                                                        </button>
 
-                                                </td>
-                                                <td
-                                                    class="border border-nu-gray-200 text-center text-lg px-4 font-semibold text-nu-green">
-                                                    12</td>
-                                                <td class="border border-nu-gray-200 px-4 text-center">
-                                                    <div class="flex">
-                                                        <span class="w-full">10</span>
-                                                        <span class="w-full">8</span>
-                                                        <span class="w-full">15</span>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            <?php endfor; ?>
-                                            <tr class="h-12 text-sm">
-                                                <td class="text-right px-4 font-semibold border border-nu-gray-200">
-                                                    {{ __('Average') }}</td>
-                                                <td
-                                                    class="text-center px-4 font-semibold bg-nu-green text-nu-primary text-lg border border-nu-gray-200">
-                                                    16</td>
-                                                <td class="text-center px-4 font-semibold border border-nu-gray-200">
-                                                    <div class="flex">
-                                                        <span class="w-full">12</span>
-                                                        <span class="w-full">6</span>
-                                                        <span class="w-full">16</span>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                            <tr class="h-12 text-sm border-l border-b border-r border-nu-gray-200" x-show="!isOpen">
-                                <td class="text-right px-4 font-semibold">{{ __('Average') }}</td>
-                                <td
-                                    class="text-center px4 font-semibold bg-nu-green text-nu-primary text-lg border-l border-r border-nu-gray-200">
-                                    16</td>
-                                <td class="text-center px-4 font-semibold">12</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                                    </td>
+                                                    <td
+                                                        class="border border-nu-gray-200 text-center text-lg px-4 font-semibold text-nu-green">
+                                                        12</td>
+                                                    <td class="border border-nu-gray-200 px-4 text-center hidden xl:table-cell"
+                                                        :class="{'!table-cell': desktopVersion}">
+                                                        <div class="flex">
+                                                            <span class="w-full">10</span>
+                                                            <span class="w-full">8</span>
+                                                            <span class="w-full">15</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <?php endfor; ?>
+                                                <tr class="h-12 text-sm">
+                                                    <td class="text-right px-4 font-semibold border border-nu-gray-200">
+                                                        {{ __('Average') }}</td>
+                                                    <td
+                                                        class="text-center px-4 font-semibold bg-nu-green text-nu-primary text-lg border border-nu-gray-200">
+                                                        16</td>
+                                                    <td class="text-center px-4 font-semibold border border-nu-gray-200 hidden xl:table-cell"
+                                                        :class="{'!table-cell': desktopVersion}">
+                                                        <div class="flex">
+                                                            <span class="w-full">12</span>
+                                                            <span class="w-full">6</span>
+                                                            <span class="w-full">16</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr class="h-12 text-sm border-l border-b border-r border-nu-gray-200" x-show="!isOpen">
+                                    <td class="text-right px-4 font-semibold">{{ __('Average') }}</td>
+                                    <td
+                                        class="text-center px4 font-semibold bg-nu-green text-nu-primary text-lg border-l border-r border-nu-gray-200">
+                                        16</td>
+                                    <td class="text-center px-4 font-semibold hidden xl:table-cell">12</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="md:hidden mt-4" x-show="isOpen">
+                        <p class="text-xs text-nu-gray-400">
+                            {{ __('Some information are hidden on mobile devices.') }}
+                        </p>
+                        <button class="btn w-full text-xs !font-normal mt-4" @click="desktopVersion = !desktopVersion"
+                            x-text="desktopVersion?'{{ __('Hide all details') }}':'{{ __('Show all details') }}'"></button>
+                    </div>
                 </div>
             </div>
         </div>
