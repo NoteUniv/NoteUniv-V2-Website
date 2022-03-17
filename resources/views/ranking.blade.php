4bb7f13df4e186305640454f29e9d7218d1f8f97
GUINGAND SOLENE <solene.guingand@hotmail.fr>
2022-03-17 17:32:40 +0100

    add translation in ranking

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -2,10 +2,17 @@
 @section('icon', __('ranking-icon'))
 
 <x-app-layout>
+    @php
+        $user = Auth::user();
+    @endphp
     <div class="box box--big col-start-2 col-span-8">
         <div class="flex flex-col md:flex-row justify-between mb-1 items-center">
             <a href="#current" class="btn text-center mb-4 px-6 md:mb-0">{{ __('Go to my rank') }}</a>
-            <a href="{{ route('profile.show') }}" class="  btn-link">{{ __('Remove me from the ranking') }}</a>
+            @if ($user->is_ranked)
+                <a href="{{ route('profile.show') }}" class="  btn-link">{{ __('Remove me from the ranking') }}</a>
+            @else
+                <a href="{{ route('profile.show') }}" class="  btn-link">{{ __('Show me in the ranking') }}</a>
+            @endif
         </div>
         <div class="sticky top-0 h-6 bg-white w-full outline outline-1 outline-white"></div>
         <table class="w-full border-collapse">
@@ -17,9 +24,6 @@
                 </tr>
             </thead>
             <tbody>
-                @php
-                    $user = Auth::user();
-                @endphp
                 @foreach ($user->averageAllStudents() as $data)
                     @if ($data['rank'])
                         <tr class="h-12 text-center" @if ($data['student_id'] == $user->student_id) x-data :class="'text-nu-green font-semibold scroll-mt-20'" id="current" @endif>
