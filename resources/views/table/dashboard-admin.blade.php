d470877dce31154e93ff87a605a142b1f066a3fd
QuentiumYT <quentin.lienhardt@gmail.com>
2022-08-30 18:01:59 +0200

    Cut file names too long admin

--- resources/views/table/dashboard-admin.blade.php
+++ resources/views/table/dashboard-admin.blade.php
@@ -1,7 +1,7 @@
 <tr class="h-12 border-b border-nu-gray-200 text-sm">
     <td class="px-4 text-xs font-semibold whitespace-nowrap">
         {{ date('d/m/Y', explode('_', basename($row), 2)[0]) }}</td>
-    <td class="px-4">{{ explode('_', basename($row), 2)[1] }}</td>
+    <td class="px-4 overflow-ellipsis overflow-hidden max-w-0">{{ explode('_', basename($row), 2)[1] }}</td>
     <td class="px-4 flex gap-x-2 h-12 items-center justify-center">
         <button class="text-nu-secondary rounded-md p-2 transition-colors duration-200 hover:text-white hover:bg-nu-secondary">
             @svg(upload-icon)
